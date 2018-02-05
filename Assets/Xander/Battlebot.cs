c86760329c82acdb56483a0e50c5d40450a89e77
Unknown <s179069@students.aie.edu.au>
2018-02-05 12:51:15 -0800

    Add Battlebot and Bullet scripts
    Also BulletBase prefab and TestBotController

---
+++ Assets/Xander/Battlebot.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Battlebot : MonoBehaviour {
+
+    public int team = 1;
+    public float maxHealth = 100.0f;
+    public float gunCooldown = 0.25f;
+    public float bulletDamage = 10.0f;
+
+    public NavMeshAgent agent;
+    public Transform bulletSpawn;
+
+    private float health;
+    private float gunHeat = 0;
+
+    //might make this public for adjustable bullet speed
+    private float bulletForce = 5000.0f;
+
+
+
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+    private void Update()
+    {
+        gunHeat = Mathf.Clamp(gunHeat - Time.deltaTime, 0, gunCooldown);
+    }
+
+
+
+    public void Damage(float amt)
+    {
+        health -= amt;
+        health = Mathf.Clamp(health, 0, maxHealth);
+        if(health <= 0)
+        {
+            Kill();
+        }
+    }
+
+    public float GetHealth()
+    {
+        return health;
+    }
+
+    public void MoveTo(Vector3 pos)
+    {
+        agent.SetDestination(pos);
+    }
+
+    public void LookAt(Vector3 pos)
+    {
+        transform.LookAt(pos);
+    }
+
+    public void Shoot(GameObject bulletPrefab)
+    {
+        if(gunHeat == 0)
+        {
+            gunHeat = gunCooldown;
+            GameObject b = Instantiate(bulletPrefab);
+            b.transform.position = bulletSpawn.position;
+            b.transform.rotation = bulletSpawn.rotation;
+            Bullet bscript = b.GetComponent<Bullet>();
+            bscript.damage = bulletDamage;
+            b.GetComponent<Rigidbody>().AddForce(b.transform.forward * bulletForce);
+            //will make longer eventually, here in case bullet escapes map
+            Destroy(b, 10);
+        }
+    }
+
+
+    
+    private void Kill()
+    {
+        Destroy(gameObject);
+    }
+
+}
