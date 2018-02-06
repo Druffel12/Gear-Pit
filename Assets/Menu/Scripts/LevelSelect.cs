236403ff44570d59d760c69d1505361cd28b8c17
Unknown <Druffel12@gmail.com>
2018-02-06 10:54:40 -0800

    Seperate team selection and size added

---
+++ Assets/Menu/Scripts/LevelSelect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public GameObject loadingImage;
+
+    public void LoadLevelSelect(string LevelSelect)
+    {
+        SceneManager.LoadScene(LevelSelect);
+    }
+}
