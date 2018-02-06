c4fe2cb3d16a3ceb13eebc2fa0af7b6b3ff7ebe2
Unknown <Druffel12@gmail.com>
2018-02-06 15:03:16 -0800

    initialize team arrays

--- Assets/Menu/Scripts/BotValueHolder.cs
+++ Assets/Menu/Scripts/BotValueHolder.cs
@@ -13,6 +13,55 @@ public class BotValueHolder : MonoBehaviour {
     public int Bot22;
     public int Bot23;
     public int Bot24;
+
+    public GameObject BotType1;
+    public GameObject BotType2;
+    public GameObject BotType3;
+    public GameObject BotType4;
+
+    public void PopulateTeamArrays()
+     {
+
+        StaticBotList.team1 = new GameObject[Bot11 + Bot12 + Bot13 + Bot14];
+        StaticBotList.team2 = new GameObject[Bot21 + Bot22 + Bot23 + Bot24];
+
+        int i = 0;
+        for (i = i; i < Bot11; ++i)
+        {
+            StaticBotList.team1[i] = BotType1;
+        }
+        for (i = i; i <Bot12 + Bot11; ++i)
+        {
+            StaticBotList.team1[i] = BotType2;
+        }
+        for (i = i; i <Bot13 + Bot12 + Bot11; ++i)
+        {
+            StaticBotList.team1[i] = BotType3;
+        }
+        for (i = i; i <Bot14 + Bot13 + Bot12 + Bot11; ++i)
+        {
+            StaticBotList.team1[i] = BotType4;
+        }
+        i = 0;
+        for (i = i; i < Bot21; ++i)
+        {
+            StaticBotList.team2[i] = BotType1;
+        }
+        for (i = i; i < Bot22 + Bot21; ++i)
+        {
+            StaticBotList.team2[i] = BotType2;
+        }
+        for (i = i; i < Bot23 + Bot22 + Bot21; ++i)
+        {
+            StaticBotList.team2[i] = BotType3;
+        }
+        for (i = i; i < Bot24 + Bot23 + Bot22 + Bot21; ++i)
+        {
+            StaticBotList.team2[i] = BotType4;
+        }
+         
+    }
+        
     // Use this for initialization
     void Start()
     {
@@ -21,16 +70,14 @@ public class BotValueHolder : MonoBehaviour {
     public void UpdateTotal1()
     {
         Bot11++;
-        Team1[] = Bot11; 
     }
     public void UpdateTotal2()
     {
         Bot12++;
-        Bot12; 
     }
     public void UpdateTotal3()
     {
-        Bot13++;
+        Bot13++;    
     }
     public void UpdateTotal4()
     {
