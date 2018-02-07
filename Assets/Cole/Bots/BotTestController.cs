3f2f1fba08e04df031853c995559dfb11f34a9e4
Unknown <Druffel12@gmail.com>
2018-02-06 17:02:57 -0800

    start working on bots

---
+++ Assets/Cole/Bots/BotTestController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotTestController : MonoBehaviour {
+
+    public GameObject Bullet;
+    public Battlebot BotTest;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        BotTest.Shoot(Bullet);
+        BotTest.MoveTo(transform.position + transform.forward);
+	}
+}
