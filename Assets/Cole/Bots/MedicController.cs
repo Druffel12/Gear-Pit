fc18cb6e2bdcdc8ec7ac8074a1b6ca1f42793d3e
Unknown <Druffel12@gmail.com>
2018-02-12 17:33:26 -0800

    start work on medic

---
+++ Assets/Cole/Bots/MedicController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedicController : MonoBehaviour
+{
+
+    public Battlebot bot;
+    private Transform ally = null;
+    private LevelManager manager;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+        if (ally == null)
+        {
+            ally = manager.FindClosestBotTo(transform.position, bot.team);
+        }
+        	
+	}
+}
